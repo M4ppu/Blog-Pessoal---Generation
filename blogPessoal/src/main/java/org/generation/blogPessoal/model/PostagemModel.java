5dc9bb4b4e4d122c8d3546ccc8d03baf3eccafe0
Luiza <luiza-97@hotmail.com>
2022-08-02 14:02:00 -0300

    Adicionando nova tabela 'tema'

--- blogPessoal/src/main/java/org/generation/blogPessoal/model/PostagemModel.java
+++ blogPessoal/src/main/java/org/generation/blogPessoal/model/PostagemModel.java
@@ -6,6 +6,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
@@ -13,9 +14,11 @@ import javax.validation.constraints.Size;
 
 import org.hibernate.annotations.UpdateTimestamp;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 
 @Entity
-@Table(name = "postagens")
+@Table(name = "tb_postagens")
 public class PostagemModel {
 	
 	@Id
@@ -32,6 +35,10 @@ public class PostagemModel {
 	
 	@UpdateTimestamp
 	private LocalDateTime data;
+	
+	@ManyToOne
+	@JsonIgnoreProperties("postagem")
+	private TemaModel tema;
 
 	public long getId() {
 		return id;
@@ -63,5 +70,13 @@ public class PostagemModel {
 
 	public void setData(LocalDateTime data) {
 		this.data = data;
+	}
+
+	public TemaModel getTema() {
+		return tema;
+	}
+
+	public void setTema(TemaModel tema) {
+		this.tema = tema;
 	}
 }
